cbaf41d99ec708e16c7637ba1a0d76c44a42f091
Luis Arques <luisarquescalero@gmail.com>
2024-02-21 18:35:02 +0100

    multiple tasks, image and scrollbar

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='trello-master'>
+			<nav className='navbar app'>App bar</nav>
+			<nav className='navbar board'>Board bar</nav>
+			<div className='board-columns'>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+
+        <div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
