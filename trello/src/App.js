47901c8bba497c0306358737a22f22c515f713ea
Luis Arques <luisarquescalero@gmail.com>
2024-02-21 21:01:39 +0100

    Refactoring in smaller components

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,104 +1,14 @@
+import AppBar from "./components/AppBar/AppBar";
+import BoardBar from "./components/BoardBar/BoardBar";
+import BoardContent from "./components/BoardContent/BoardContent";
+
 function App() {
 	return (
 		<div className='trello-master'>
-			<nav className='navbar app'>App bar</nav>
-			<nav className='navbar board'>Board bar</nav>
-			<div className='board-columns'>
-				<div className='column'>
-					<header>Brainstorm</header>
-					<ul>
-						<li>
-							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
-							Design and research
-						</li>
-						<li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-				<div className='column'>
-					<header>Brainstorm</header>
-					<ul>
-						<li>
-							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
-							Design and research
-						</li>
-						<li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-				<div className='column'>
-					<header>Brainstorm</header>
-					<ul>
-						<li>
-							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
-							Design and research
-						</li>
+			<AppBar />
+			<BoardBar />
+			<BoardContent />
 
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-            <li>Task 2</li>
-						<li>Task 3</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-				<div className='column'>
-					<header>Brainstorm</header>
-					<ul>
-						<li>
-							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
-							Design and research
-						</li>
-						<li>Task 2</li>
-						<li>Task 3</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-        		<div className='column'>
-					<header>Brainstorm</header>
-					<ul>
-						<li>
-							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
-							Design and research
-						</li>
-						<li>Task 2</li>
-						<li>Task 3</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-			</div>
 		</div>
 	);
 }
---
+++ trello/src/components/BoardContent/BoardContent.js
@@ -0,0 +1,122 @@
+import './BoardContent.scss';
+
+const BoardContent = () => {
+	return (
+		<>
+			<div className='board-columns'>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+								alt='Design'
+							/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+								alt='Design'
+							/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+								alt='Design'
+							/>
+							Design and research
+						</li>
+
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+								alt='Design'
+							/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+								alt='Design'
+							/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+			</div>
+		</>
+	);
+};
+
+
+export default BoardContent;
