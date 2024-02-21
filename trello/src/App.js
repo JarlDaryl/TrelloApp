7ed4e97c0777d9728e300396266f152e15bbc57b
Luis Arques <luisarquescalero@gmail.com>
2024-02-21 18:55:27 +0100

    scrollbars arregladas y redondeadas

--- trello/src/App.js
+++ trello/src/App.js
@@ -30,8 +30,63 @@ function App() {
 					</ul>
 					<footer>Add another card</footer>
 				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
+							Design and research
+						</li>
 
-        <div className='column'>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+            <li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>Brainstorm</header>
+					<ul>
+						<li>
+							<img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' alt="Design"/>
+							Design and research
+						</li>
+						<li>Task 2</li>
+						<li>Task 3</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+        		<div className='column'>
 					<header>Brainstorm</header>
 					<ul>
 						<li>
