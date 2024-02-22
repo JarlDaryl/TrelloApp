5255413c4cdfdc5e6fe7428c562013f7001d2c7b
Luis Arques <luisarquescalero@gmail.com>
2024-02-22 14:02:38 +0100

    Ordenacion de columnas y tareas + Mock data

--- trello/src/components/Column/Column.js
+++ trello/src/components/Column/Column.js
@@ -1,24 +1,27 @@
 import './Column.scss';
-import Task from '../Task/Task';
+import Card from '../Card/Card';
+import { mapOrder } from '../../utilities/sorts';
 
-const Column = () => {
+
+const Column = (props) => {
+
+const {column} = props;
+const cards = mapOrder(column.cards, column.cardOrder, 'id');
+
+
 	return (
 		<>
 			<div className='column'>
-				<header>Brainstorm</header>
-				<ul className='task-list'>
-                <Task />
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 3</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 3</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 3</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 2</li>
-					<li className='task-item'>Task 2</li>
+				<header>{column.title}</header>
+				<ul className='card-list'>
+				{cards && cards.length > 0 && cards.map((card, index) =>{
+					return(
+						<Card 
+						key={card.id}
+						card={card}
+						/>
+					)
+				})}
 				</ul>
 				<footer>Add another card</footer>
 			</div>
